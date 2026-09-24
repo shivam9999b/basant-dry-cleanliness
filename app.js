bd94b2984044959277083587f61de2252184a2bc
shivam9999b <surajnews59@gmail.com>
2026-09-24 11:33:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
    SUPABASE CONFIG
 ===================================================== */
 
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://ubsyhqkefhtskxjpjzbf.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVic3locWtlZmh0c2t4anBqemJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4Mzk4NjQsImV4cCI6MjEwNTQxNTg2NH0.00-JsYNjdmb7OV51Tly1W8A9grQ9GR9E66E3Yyas-j8";
 
 let supabaseClient = null;
 
