f84820e8a124b5ab1ae95eb3e2f771994ac609a6
shivam9999b <surajnews59@gmail.com>
2026-09-24 11:35:42 +0530

    Update track.js

--- track.js
+++ track.js
@@ -9,10 +9,10 @@
 ===================================================== */
 
 const SUPABASE_URL =
-  "";
+  "https://ubsyhqkefhtskxjpjzbf.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVic3locWtlZmh0c2t4anBqemJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4Mzk4NjQsImV4cCI6MjEwNTQxNTg2NH0.00-JsYNjdmb7OV51Tly1W8A9grQ9GR9E66E3Yyas-j8";
 
 const supabaseClient =
   window.supabase.createClient(
